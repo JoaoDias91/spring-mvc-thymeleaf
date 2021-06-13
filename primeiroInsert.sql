62347725752af9c24c3ac46ef40167be6ccf0f82
Joao paulo Dias Siqueira <joao@MacBook-Air-de-Joao.local>
2021-06-13 20:48:53 -0300

    Add novo controller

---
+++ primeiroInsert.sql
@@ -0,0 +1,5 @@
+insert into pedido (nome_produto, url_da_imagem, url_do_produto, descricaco)
+       values ("The Last of Us Part II - PlayStation 4",
+       "https://images-na.ssl-images-amazon.com/images/I/61OZeW%2BpPYL._AC_SL1000_.jpg",
+       "https://www.amazon.com.br/dp/B07ZGFHZVZ/ref=s9_acsd_al_bw_c2_x_1_i?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-3&pf_rd_r=KJN0R7DT8C0K5SFCT85N&pf_rd_t=101&pf_rd_p=7b43f1c2-0945-4ec2-800d-57e0a663d5a9&pf_rd_i=7791985011",
+       "jogo The Last of Us Part II para PS4");
